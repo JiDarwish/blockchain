f6247e3c489991f2f1892ad07740d4b1871446a3
Marie-Charlotte Eyb <marie-charlotte@gmx.de>
2018-04-06 22:14:38 +0200

    design 1

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,47 +1,69 @@
 import React, { Component } from 'react'
+import '../styles/table.css'
 
 export default class Table extends Component {
   render() {
     return (
+      <body>
       <div>
+      <div className="container">
         <table>
           <thead>
             <tr>
-              <th>timestamp</th>
-              <th>alert</th>
-              <th>status</th>
-              <th>something</th>
+            <div className="row">
+              <th>Date and Time</th>
+              <th>Company</th>
+              <th>Status</th>
+              <th>Alert</th>
+              </div>
             </tr>
           </thead>
           <tbody>
             <tr>
-              <td>time</td>
-              <td>time</td>
-              <td>time</td>
-              <td>time</td>
+            <div className="row">
+              <td>2017-03-29 14:12</td>
+              <td>A</td>
+              <td>Warning</td>
+              <td>Temperature has been too high</td>
+              </div>
             </tr>
             <tr>
-              <td>alert</td>
-              <td>alert</td>
-              <td>alert</td>
-              <td>alert</td>
+            <div className="row">
+              <td>2017-03-29 15:12</td>
+              <td>B</td>
+              <td>Info</td>
+              <td>Train arrived on time</td>
+              </div>
             </tr>
             <tr>
-              <td>status</td>
-              <td>status</td>
-              <td>status</td>
-              <td>status</td>
-
+            <div className="row">
+              <td>2017-03-29 16:12</td>
+              <td>C</td>
+              <td>Test</td>
+              <td>checkbox</td>
+              </div>
             </tr>
             <tr>
-              <td>something</td>
-              <td>something</td>
-              <td>something</td>
-              <td>something</td>
+            <div className="row">
+              <td>2017-03-29 14:12</td>
+              <td>A</td>
+              <td>Warning</td>
+              <td>Red Dot</td>
+              </div>
+            </tr>
+             <tr>
+             <div className="row">
+              <td>2017-03-29 15:12</td>
+              <td>B</td>
+              <td>Info</td>
+              <td>Checkbox unchecked</td>
+              </div>
             </tr>
           </tbody>
         </table>
+        </div>
       </div>
+      </body>
     )
   }
 }

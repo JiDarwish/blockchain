2635de798a71862f25b4a61596768d248e09fe8f
JiDarwish <ji.darwish98@gmail.com>
2018-04-07 00:11:14 +0200

    Added way for multiple charts

--- Client/src/App.js
+++ Client/src/App.js
@@ -3,23 +3,34 @@ import React, { Component } from 'react';
 import Table from './components/Table';
 import Map from './components/GoogleMap';
 import './styles/app.css'
-import SparklineChart from './components/SparklineChart';
+import Sparklines from './components/sparklines/Sparklines';
+import Header from './components/Header';
+
+
+const fakeData = [
+  [22, 55, 77, 33, 55, 77, 33],
+  [22, 55, 77, 33, 55, 77, 33],
+  [22, 55, 77, 33, 55, 77, 33],
+  [22, 55, 77, 33, 55, 77, 33],
+
+]
 
 class App extends Component {
   render() {
     return (
-<div className="app-container">
-      <div className="flex-container">
-        <Map
-          googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyD4i89nRz-1SGc0NlS3LaH_ucuK0En4DkY"
-          loadingElement={<div style={{ height: `100%` }} />}
-          containerElement={<div style={{ height: `400px`, width: `50%` }} />}
-          mapElement={<div style={{ height: `100%` }} />}
-          isMarkerShown
-        />
-        <Table />
+      <div className="app-container">
+        <Header />
+        <div className="flex-container">
+          <Map
+            googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyD4i89nRz-1SGc0NlS3LaH_ucuK0En4DkY"
+            loadingElement={<div style={{ height: `100%` }} />}
+            containerElement={<div style={{ height: `400px`, width: `50%` }} />}
+            mapElement={<div style={{ height: `100%` }} />}
+            isMarkerShown
+          />
+          <Table />
         </div>
-        <SparklineChart />
+        <Sparklines data={fakeData} />
       </div>
     );
   }
--- Client/src/components/Table.js
+++ Client/src/components/Table.js
@@ -4,66 +4,52 @@ import '../styles/table.css'
 export default class Table extends Component {
   render() {
     return (
-      <body>
       <div>
-      <div className="container">
-        <table>
-          <thead>
-            <tr>
-            <div className="row">
-              <th>Date and Time</th>
-              <th>Company</th>
-              <th>Status</th>
-              <th>Alert</th>
-              </div>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-            <div className="row">
-              <td>2017-03-29 14:12</td>
-              <td>A</td>
-              <td><a class='btn btn-danger' href='#'>Warning</a></td>
-              <td>Temperature has been too high</td>
-              </div>
-            </tr>
-            <tr>
-            <div className="row">
-              <td>2017-03-29 15:12</td>
-              <td>B</td>
-              <td>Info</td>
-              <td>Train arrived on time</td>
-              </div>
-            </tr>
-            <tr>
-            <div className="row">
-              <td>2017-03-29 16:12</td>
-              <td>C</td>
-              <td>Test</td>
-              <td>checkbox</td>
-              </div>
-            </tr>
-            <tr>
-            <div className="row">
-              <td>2017-03-29 14:12</td>
-              <td>A</td>
-              <td>Warning</td>
-              <td>Red Dot</td>
-              </div>
-            </tr>
-             <tr>
-             <div className="row">
-              <td>2017-03-29 15:12</td>
-              <td>B</td>
-              <td>Info</td>
-              <td>Checkbox unchecked</td>
-              </div>
-            </tr>
-          </tbody>
-        </table>
+        <div className="container">
+          <table>
+            <thead>
+              <tr>
+                <th>Date and Time</th>
+                <th>Company</th>
+                <th>Status</th>
+                <th>Alert</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>2017-03-29 14:12</td>
+                <td>A</td>
+                <td><a className='btn btn-danger' href='#'>Warning</a></td>
+                <td>Temperature has been too high</td>
+              </tr>
+              <tr>
+                <td>2017-03-29 15:12</td>
+                <td>B</td>
+                <td>Info</td>
+                <td>Train arrived on time</td>
+              </tr>
+              <tr>
+                <td>2017-03-29 16:12</td>
+                <td>C</td>
+                <td>Test</td>
+                <td>checkbox</td>
+              </tr>
+              <tr>
+                <td>2017-03-29 14:12</td>
+                <td>A</td>
+                <td>Warning</td>
+                <td>Red Dot</td>
+              </tr>
+              <tr>
+                <td>2017-03-29 15:12</td>
+                <td>B</td>
+                <td>Info</td>
+                <td>Checkbox unchecked</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </div>
-      </body>
     )
   }
 }
